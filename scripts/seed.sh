6836ba9a6f2e13d61fd406ec05a08aed4b4e4550
Robert Sigler <robert.sigler@thomsonreuters.com>
2023-05-16 13:27:26 -0500

    Add views with velocity

--- scripts/seed.sh
+++ scripts/seed.sh
@@ -1,4 +1,6 @@
 # curl -X POST -H "Content-Type: application/json" localhost:8080/books -d \
 #   '{"name":"Words of Radiance", "isbn":"9781250166531", "genreId":"1"}'
-curl -X PUT -H "Content-Type: application/json" localhost:8080/books -d \
-  '{"id": "3", "name":"Words of Radiance", "isbn":"9781250166531", "genreId":"1"}'
+# curl -X PUT -H "Content-Type: application/json" localhost:8080/books -d \
+#   '{"id": "3", "name":"Words of Radiance", "isbn":"9781250166531", "genreId":"1"}'
+curl -X PUT -H "Content-Type: application/json" localhost:8080/genres -d \
+  '{"name":"Fantasy"}'
--- src/main/java/example/micronaut/BookController.java
+++ src/main/java/example/micronaut/BookController.java
@@ -3,17 +3,22 @@ package example.micronaut;
 import example.micronaut.domain.Book;
 import example.micronaut.domain.Genre;
 import io.micronaut.http.HttpResponse;
+import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Body;
 import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Consumes;
 import io.micronaut.http.annotation.Delete;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.Post;
 import io.micronaut.http.annotation.Put;
 import io.micronaut.scheduling.TaskExecutors;
 import io.micronaut.scheduling.annotation.ExecuteOn;
+import io.micronaut.views.View;
 
 import javax.persistence.PersistenceException;
 import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 import java.net.URI;
 import java.util.List;
 
@@ -36,6 +41,20 @@ class BookController {
                 .orElse(null); // <5>
     }
 
+    @View("bookscreate")
+    @Get("/create")
+    public Map<String, Object> create() {
+        return createModelWithBlankValues();
+    }
+
+    private Map<String, Object> createModelWithBlankValues() {
+        final Map<String, Object> model = new HashMap<>();
+        model.put("title", "");
+        model.put("isbn", "");
+        model.put("genreId", "");
+        return model;
+    }
+
     @Put // <6>
     HttpResponse<?> update(@Body @Valid BookUpdateCommand command) { // <7>
         int numberOfEntitiesUpdated = bookRepository.update(command.getId(), command.getName(), command.getIsbn(), command.getGenreId());
@@ -50,7 +69,8 @@ class BookController {
         return bookRepository.findAll(args);
     }
 
-    @Post // <10>
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    @Post()
     HttpResponse<Book> save(@Body @Valid BookSaveCommand cmd) {
         Book book = bookRepository.save(cmd.getName(), cmd.getIsbn(), cmd.getGenreId());
 
@@ -59,7 +79,7 @@ class BookController {
                 .headers(headers -> headers.location(location(book.getId())));
     }
 
-    @Post("/ex") // <11>
+    @Post("/ex")
     HttpResponse<Book> saveExceptions(@Body @Valid BookSaveCommand cmd) {
         try {
             Book book = bookRepository.saveWithException(cmd.getName(), cmd.getIsbn(), cmd.getGenreId());
@@ -71,7 +91,7 @@ class BookController {
         }
     }
 
-    @Delete("/{id}") // <12>
+    @Delete("/{id}")
     HttpResponse<?> delete(Long id) {
         bookRepository.deleteById(id);
         return HttpResponse.noContent();
